07b7ac6fd3ae349083ae2c4eaf6c110ff57a93b1
Harun Akgul <46harunakgul@gmail.com>
2022-08-20 17:26:53 +0300

    Adding id command HTML file and The desired commands were written to the js file.

---
+++ index.js
@@ -0,0 +1,23 @@
+
+    var randomNumber1 = Math.random();
+    randomNumber1 = randomNumber1*6;
+    randomNumber1 = Math.floor(randomNumber1) + 1;
+    document.getElementById("player1img").src = `images/dice${randomNumber1}.png`;
+
+    var randomNumber2 = Math.random();
+    randomNumber2 = randomNumber2*6;
+    randomNumber2 = Math.floor(randomNumber2) + 1;
+    document.getElementById("player2img").src = `images/dice${randomNumber2}.png`;
+
+
+    if(randomNumber1 > randomNumber2){
+        document.getElementById("title") = "Player 1 Win";
+    }
+
+    else if(randomNumber2 > randomNumber1){
+        document.getElementById("title") = "Player 2 Win";
+    }
+
+    else{
+        document.getElementById("title") = "Draw";
+    }
